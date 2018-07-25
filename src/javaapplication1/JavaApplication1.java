bd776e99f52fbfb596675abaa5af470fa1ab1c11
LabingXEON <LabingXEON@LabingXEON4-PC>
2018-07-25 11:20:08 -0500

    ejemplo 1 del hola mundo

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,18 @@
+
+package javaapplication1;
+
+/**
+ *
+ * @author LabingXEON
+ * @version 0.1
+ * Ejemplo 1 bases de datos
+ */
+public class JavaApplication1 {
+
+   
+    public static void main(String[] args) {
+        
+        System.out.println("Hola Mundo");
+    }
+    
+}
